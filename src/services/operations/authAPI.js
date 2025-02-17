eaab993afe50f875d3328f22404645a9afb94306
TusharGidde <tushargidde00730@gmail.com>
2025-02-17 13:10:18 +0530

    Update authAPI.js

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -89,38 +89,38 @@ export function signUp(
 }
 
 export function login(email, password, navigate) {
-  // return async (dispatch) => {
-  //   const toastId = toast.loading("Loading...")
-  //   dispatch(setLoading(true))
-  //   try {
-  //     const response = await apiConnector("POST", LOGIN_API, {
-  //       email,
-  //       password,
-  //     })
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...")
+    dispatch(setLoading(true))
+    try {
+      const response = await apiConnector("POST", LOGIN_API, {
+        email,
+        password,
+      })
 
-  //     console.log("LOGIN API RESPONSE............", response)
+      console.log("LOGIN API RESPONSE............", response)
 
-  //     if (!response.data.success) {
-  //       throw new Error(response.data.message)
-  //     }
-  //     dispatch(setProgress(100))
-  //     toast.success("Login Successful")
-  //     dispatch(setToken(response.data.token))
-  //     const userImage = response.data?.user?.image
-  //       ? response.data.user.image
-  //       : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.user.firstName} ${response.data.user.lastName}`
-  //     dispatch(setUser({ ...response.data.user, image: userImage }))
-  //     localStorage.setItem("user", JSON.stringify(response.data.user))
-  //     localStorage.setItem("token", JSON.stringify(response.data.token))
-      // navigate("/dashboard/my-profile")
-  //   } catch (error) {
-  //     dispatch(setProgress(100))
-  //     console.log("LOGIN API ERROR............", error)
-  //     toast.error(error.response.data.message)
-  //   }
-  //   dispatch(setLoading(false))
-  //   toast.dismiss(toastId)
-  // }
+      if (!response.data.success) {
+        throw new Error(response.data.message)
+      }
+      dispatch(setProgress(100))
+      toast.success("Login Successful")
+      dispatch(setToken(response.data.token))
+      const userImage = response.data?.user?.image
+        ? response.data.user.image
+        : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.user.firstName} ${response.data.user.lastName}`
+      dispatch(setUser({ ...response.data.user, image: userImage }))
+      localStorage.setItem("user", JSON.stringify(response.data.user))
+      localStorage.setItem("token", JSON.stringify(response.data.token))
+      navigate("/dashboard/my-profile")
+    } catch (error) {
+      dispatch(setProgress(100))
+      console.log("LOGIN API ERROR............", error)
+      toast.error(error.response.data.message)
+    }
+    dispatch(setLoading(false))
+    toast.dismiss(toastId)
+  }
 
 
 }
